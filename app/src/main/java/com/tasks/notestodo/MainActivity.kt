6547cc595077692149e2eb69fb01013fd613db5b
Vlad <vladandryschenko98@gmail.com>
2024-06-18 00:17:42 +0300

    init

---
+++ app/src/main/java/com/tasks/notestodo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tasks.notestodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
