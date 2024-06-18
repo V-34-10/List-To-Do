bd5233b656472e1fa321de951608bcc949029378
Vlad <vladandryschenko98@gmail.com>
2024-06-19 00:59:40 +0300

    Ui MainActivity: recyclerList, fab

--- app/src/main/java/com/tasks/notestodo/ui/MainActivity.kt
+++ app/src/main/java/com/tasks/notestodo/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.tasks.notestodo
+package com.tasks.notestodo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tasks.notestodo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
