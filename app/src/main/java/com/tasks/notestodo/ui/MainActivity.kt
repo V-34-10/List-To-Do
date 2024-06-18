bdf46bd497068d094d5adeb18c55062f3961fa9f
Vlad <vladandryschenko98@gmail.com>
2024-06-19 01:38:06 +0300

    MainActivity: initRecycler, initFab

--- app/src/main/java/com/tasks/notestodo/ui/MainActivity.kt
+++ app/src/main/java/com/tasks/notestodo/ui/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.tasks.notestodo.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.tasks.notestodo.R
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.tasks.notestodo.MyApplication
+import com.tasks.notestodo.databinding.ActivityMainBinding
+import com.tasks.notestodo.ui.adapters.TasksAdapter
+import com.tasks.notestodo.ui.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding = ActivityMainBinding.inflate(layoutInflater)
+
+    private val viewModel: MainViewModel by viewModels {
+        MainViewModel.MainViewModelFactory((application as MyApplication).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initRecycler()
+        initFab()
+    }
+
+    private fun initRecycler() {
+        binding.list.layoutManager = LinearLayoutManager(this)
+        val adapter = TasksAdapter(layoutInflater,
+            { task ->
+                val i = Intent(this, TaskActivity::class.java)
+                i.putExtra("task_id", task.id)
+                startActivity(i)
+            },
+            { task ->
+                viewModel.delete(task)
+            })
+        binding.list.adapter = adapter
+
+        viewModel.notes.observe(this) {
+            adapter.submitList(it)
+        }
+    }
+
+    private fun initFab() {
+        binding.fab.setOnClickListener { startActivity(Intent(this, TaskActivity::class.java)) }
     }
 }
