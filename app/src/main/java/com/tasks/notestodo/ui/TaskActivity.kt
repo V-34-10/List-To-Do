b8ae961520c74958e72b06b7508cdc8508441eb6
Vlad <vladandryschenko98@gmail.com>
2024-06-19 01:54:06 +0300

    TaskActivity: viewTask, editTask, clearTextInput

--- app/src/main/java/com/tasks/notestodo/ui/TaskActivity.kt
+++ app/src/main/java/com/tasks/notestodo/ui/TaskActivity.kt
@@ -1,12 +1,71 @@
 package com.tasks.notestodo.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.tasks.notestodo.R
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import com.tasks.notestodo.MyApplication
+import com.tasks.notestodo.databinding.ActivityTaskBinding
+import com.tasks.notestodo.model.Task
+import com.tasks.notestodo.ui.viewmodels.TaskViewModel
+import java.util.Date
 
 class TaskActivity : AppCompatActivity() {
+    private val binding = ActivityTaskBinding.inflate(layoutInflater)
+
+    private val viewModel: TaskViewModel by viewModels() {
+        TaskViewModel.NoteViewModelFactory((application as MyApplication).repository)
+    }
+    private var editMode = false
+    private val oldNote = MutableLiveData<Task>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_task)
+        setContentView(binding.root)
+
+        findEditTask()
+        initControlButton()
+    }
+
+    private fun findEditTask() {
+        val id = intent.getLongExtra("task_id", -1L)
+        editMode = id != -1L
+
+        if (editMode)
+            viewModel.getTaskById(oldNote, id)
+
+        oldNote.observe(this) {
+            it?.let {
+                binding.title.editText?.setText(it.title)
+                binding.text.editText?.setText(it.text)
+            } ?: throw IllegalArgumentException("task_id is incorrect")
+        }
+    }
+
+    private fun initControlButton() {
+        binding.save.setOnClickListener {
+            val date = Date()
+            if (editMode) {
+                oldNote.value?.let {
+                    it.title = binding.title.editText?.text.toString()
+                    it.text = binding.text.editText?.text.toString()
+                    it.updated = date
+                    viewModel.updateTask(it)
+                }
+            } else {
+                val task = Task(
+                    title = binding.title.editText?.text.toString(),
+                    text = binding.text.editText?.text.toString(),
+                    created = date,
+                    updated = date
+                )
+                viewModel.addTask(task)
+            }
+            finish()
+        }
+
+        binding.clear.setOnClickListener {
+            binding.title.editText?.text?.clear()
+            binding.text.editText?.text?.clear()
+        }
     }
 }
