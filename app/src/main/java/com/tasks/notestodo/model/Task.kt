41658070ff6490007654655cac102cceffde6e86
Vlad <vladandryschenko98@gmail.com>
2024-06-19 15:16:37 +0300

    Task: add property color

--- app/src/main/java/com/tasks/notestodo/model/Task.kt
+++ app/src/main/java/com/tasks/notestodo/model/Task.kt
@@ -9,6 +9,7 @@ data class Task(
     @PrimaryKey(autoGenerate = true) var id: Long = 0L,
     var title: String = "",
     var text: String = "",
+    var color: Int = android.R.color.white,
     var created: Date = Date(),
     var updated: Date = Date()
 )
