833dc5d707bf62dd992e19eb9931d0aad6460760
Vlad <vladandryschenko98@gmail.com>
2024-06-19 19:26:51 +0300

    add dependencies: cardView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,9 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-common-java8:2.6.1")
 
+    //card-view
+    implementation("androidx.cardview:cardview:1.0.0")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
