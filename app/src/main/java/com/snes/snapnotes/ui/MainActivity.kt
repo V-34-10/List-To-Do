b116e9af6b2b1b539d02260c63a49cd0d265372c
Vlad <vladandryschenko98@gmail.com>
2024-06-20 23:01:09 +0300

    UI MainActivity: add recycler, appbar, searchbar, filters

--- app/src/main/java/com/snes/snapnotes/ui/MainActivity.kt
+++ app/src/main/java/com/snes/snapnotes/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.snes.snapnotes
+package com.snes.snapnotes.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.snes.snapnotes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
