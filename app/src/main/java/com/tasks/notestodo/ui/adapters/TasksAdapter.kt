8640da689ce1de358f3ee0bcc979fd30abea0822
Vlad <vladandryschenko98@gmail.com>
2024-06-20 15:05:06 +0300

    TaskAdapter: add change favoriteImage

--- app/src/main/java/com/tasks/notestodo/ui/adapters/TasksAdapter.kt
+++ app/src/main/java/com/tasks/notestodo/ui/adapters/TasksAdapter.kt
@@ -4,6 +4,7 @@ import android.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
@@ -73,6 +74,14 @@ class TasksAdapter(
         val cardView = holder.itemView.findViewById<CardView>(R.id.cardView)
         cardView.setCardBackgroundColor(backgroundColor)
         cardView.radius = 16f
+
+        if (task.isFavorite) {
+            holder.itemView.findViewById<ImageButton>(R.id.favoriteButton)
+                .setImageResource(R.drawable.baseline_favorite_24)
+        } else {
+            holder.itemView.findViewById<ImageButton>(R.id.favoriteButton)
+                .setImageResource(R.drawable.baseline_favorite_border_24)
+        }
     }
 
     object UserDiffCallback : DiffUtil.ItemCallback<Task>() {
