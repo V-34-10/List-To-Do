62fa33f42b81fc6ff4ae60cde32222fbaa51b248
Vlad <vladandryschenko98@gmail.com>
2024-06-24 18:21:50 +0300

    Note: fix remove color

--- app/src/main/java/com/snes/snapnotes/model/Note.kt
+++ app/src/main/java/com/snes/snapnotes/model/Note.kt
@@ -9,7 +9,6 @@ data class Note(
     @PrimaryKey(autoGenerate = true) var id: Long = 0L,
     var title: String = "",
     var text: String = "",
-    var color: Int = android.R.color.white,
     var created: Date = Date(),
     var updated: Date = Date(),
     var isFavorite: Boolean = false
