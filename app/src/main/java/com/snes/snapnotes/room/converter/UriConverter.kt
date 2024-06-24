d7176817cb4249d290e59b989686c25af7a118e4
Vlad <vladandryschenko98@gmail.com>
2024-06-25 00:18:45 +0300

    UriConverter: fromUri, toUri

---
+++ app/src/main/java/com/snes/snapnotes/room/converter/UriConverter.kt
@@ -0,0 +1,16 @@
+package com.snes.snapnotes.room.converter
+
+import android.net.Uri
+import androidx.room.TypeConverter
+
+class UriConverter {
+    @TypeConverter
+    fun fromUri(uri: Uri?): String? {
+        return uri?.toString()
+    }
+
+    @TypeConverter
+    fun toUri(uriString: String?): Uri? {
+        return if (uriString != null) Uri.parse(uriString) else null
+    }
+}
