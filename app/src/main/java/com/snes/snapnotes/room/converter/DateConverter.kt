b3e17a6fa9272a281faf78bfb01ebb56bd308560
Vlad <vladandryschenko98@gmail.com>
2024-06-24 17:12:46 +0300

    Converter: add dateToTimestamp, fromTimestamp

---
+++ app/src/main/java/com/snes/snapnotes/room/converter/DateConverter.kt
@@ -0,0 +1,12 @@
+package com.snes.snapnotes.room.converter
+
+import androidx.room.TypeConverter
+import java.util.Date
+
+class DateConverter {
+    @TypeConverter
+    fun fromTimestamp(value: Long?) = value?.let { Date(it) }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?) = date?.time
+}
